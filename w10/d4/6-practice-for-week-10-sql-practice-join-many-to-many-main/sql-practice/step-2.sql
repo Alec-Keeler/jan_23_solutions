a76154e45a06f4b654b13eee311a9b49cf965414
Alec Keeler <alec.keeler@gmail.com>
2023-03-30 16:33:38 -0500

    demo complete

--- w10/d4/6-practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-2.sql
+++ w10/d4/6-practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-2.sql
@@ -2,3 +2,10 @@
 -- Just like with one-to-many relationships, you can filter one table by any
 -- associated data on a conected table.
 -- Your code here
+
+
+SELECT musicians.first_name, musicians.last_name
+FROM musicians 
+JOIN musician_instruments ON (musicians.id = musician_instruments.musician_id)
+JOIN instruments ON (musician_instruments.instrument_id = instruments.id)
+where instruments.type = 'piano';
